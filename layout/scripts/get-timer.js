3b15a96a3c880db641a55b66ae8a9cdb7ee694fc
virtualpanda <1905228@kiit.ac.in>
2021-06-26 14:13:53 +0530

    minor changes

--- layout/scripts/get-timer.js
+++ layout/scripts/get-timer.js
@@ -8,6 +8,15 @@ var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
 var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+if(hours<10)
+    hours="0"+hours;
+if(days<10)
+    days="0"+days;
+if(minutes<10)
+    minutes="0"+minutes;
+
+if(seconds<10)
+    seconds="0"+seconds;
 if (timeleft < 0) {
 text.innerHTML="Workshop has started"
 }
