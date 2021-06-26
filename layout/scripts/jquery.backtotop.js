eb5b8903f57a3bd10e45fcbf88990ec87b628d7f
virtualpanda <1905228@kiit.ac.in>
2021-06-26 11:02:31 +0530

    p

--- layout/scripts/jquery.backtotop.js
+++ layout/scripts/jquery.backtotop.js
@@ -12,11 +12,7 @@ jQuery("#backtotop").click(function () {
         scrollTop: 0
     }, 600);
 });
-jQuery("#backtotop").click(function () {
-    jQuery("body,html").animate({
-        scrollTop: 0
-    }, 600);
-});
+
 jQuery(window).scroll(function () {
     if (jQuery(window).scrollTop() > 150) {
         jQuery("#backtotop").addClass("visible");
